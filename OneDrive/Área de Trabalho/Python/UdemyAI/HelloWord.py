9fec3eba5bc55cd14052e851b1f331abb6dd9155
Said Saade <said-saade@hotmail.com>
2025-09-28 18:25:55 -0300

    Envio do Projeto

---
+++ OneDrive/Área de Trabalho/Python/UdemyAI/HelloWord.py
@@ -0,0 +1,9 @@
+# Solicita dois números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realiza a soma
+resultado = num1 + num2
+
+# Exibe o resultado
+print("A soma de", num1, "e", num2, "é:", resultado)
